2e35146a1bf1630070bd139346035889dfaca03e
Adam Nevrlý <anevrly@gmail.com>
2022-11-17 15:52:04 +0100

    Small refactoring

---
+++ myBuildableCodeParcel/src/index.ts
@@ -0,0 +1,11 @@
+import { fabric } from "fabric";
+import { MyInt } from "./myDataStructures";
+
+
+console.log("[DEBUG] Initialzing Fabric");
+var canvas = new fabric.Canvas('myCanvas');
+
+console.log("[DEBUG] Creating a first MyInt");
+var myInt1 = new MyInt(12, 100, 100);
+myInt1.drawObject(canvas);
+console.log("[DEBUG] After drawing a the first MyInt");
