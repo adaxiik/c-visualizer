ce7128dd5b7c4afb93a12f1009dd5dc94f34e7a5
Adam Nevrlý <anevrly@gmail.com>
2022-11-17 15:25:28 +0100

    Adding a simple myInt structure

---
+++ myBuildableCodeParcel/src/dataStructuresTest.ts
@@ -0,0 +1,30 @@
+//For now moved into the "tsTextIndex.ts" file
+/*import { fabric } from "fabric";
+
+class MyInt {
+    value: number;
+
+    //Values for later drawing in Fabric
+    fabricObject: fabric.Circle;
+    color: string;
+    radius: number;
+    posTop: number;
+    posLeft: number;
+
+    constructor(setValue: number, setPosTop: number, setPosLeft: number) {
+        //Default values
+        this.color = '#33ccff';
+        this.radius = 30;
+        
+        //Set values
+        this.value = setValue;
+        this.posTop = setPosTop;
+        this.posLeft = setPosLeft;
+        
+        this.fabricObject = new fabric.Circle({ radius: this.radius, fill: this.color, top: this.posTop, left: this.posLeft })
+    }
+
+    getObject() {
+        return this.fabricObject;
+    }
+}*/
--- myBuildableCodeParcel/src/tsTextIndex.ts
+++ myBuildableCodeParcel/src/tsTextIndex.ts
@@ -1,6 +1,41 @@
 import { fabric } from "fabric";
+//import * from "./dataStructuresTest.ts"; //For now moved here
+class MyInt {
+    value: number;
 
-console.log("[debug] Before drawing a circle");
+    //Values for later drawing in Fabric
+    fabricGroup: fabric.Group;
+    fabricObject: fabric.Circle;
+    fabricText: fabric.Text;
+    color: string;
+    radius: number;
+    posTop: number;
+    posLeft: number;
+
+    constructor(setValue: number, setPosTop: number, setPosLeft: number) {
+        //Default values
+        this.color = '#33ccff';
+        this.radius = 30;
+        
+        //Set values
+        this.value = setValue;
+        this.posTop = setPosTop;
+        this.posLeft = setPosLeft;
+        
+        this.fabricObject = new fabric.Circle({ radius: this.radius, fill: this.color, top: this.posTop, left: this.posLeft })
+        this.fabricText = new fabric.Text(this.value.toString(), { left: this.fabricObject.left, top: this.fabricObject.top, fill: 'black' });
+        this.fabricGroup = new fabric.Group([this.fabricObject, this.fabricText]);
+    }
+
+    drawObject(drawIntoCanvas: fabric.Canvas) {
+        drawIntoCanvas.add(this.fabricGroup);
+    }
+}
+
+console.log("[debug] Initialzing Fabric");
 var canvas = new fabric.Canvas('myCanvas');
-canvas.add(new fabric.Circle({ radius: 30, fill: '#33ccff', top: 100, left: 100 }));
-console.log("[debug] After drawing a circle");
+
+console.log("[debug] Creating a first myInt");
+var myInt1 = new MyInt(12, 100, 100);
+myInt1.drawObject(canvas);
+console.log("[debug] After drawing a the first myInt");
